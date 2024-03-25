8f0ea2b75b6d8bc129b888ffde958e678b408efe
sana arshad <160604463+Aaira123@users.noreply.github.com>
2024-03-25 12:47:02 -0700

    module

---
+++ app.ts
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([{
+    name: "Age",
+    type: "number",
+    message: "Enter your age;",
+}])
+
+console.log("insha Allah in " + (60 - answer.Age) + "years you will be 60 years old");
